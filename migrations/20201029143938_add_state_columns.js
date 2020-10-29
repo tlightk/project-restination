9a83898c92b9ef0762d83a93f943e9a4931f7cee
tlightk <klayxi@gmail.com>
2020-10-29 15:00:18 +0900

    expanded locations table to include initial columns

---
+++ migrations/20201029143938_add_state_columns.js
@@ -0,0 +1,25 @@
+exports.up = function(knex) {
+  return knex.schema.createTable("locations", (table) => {
+    table.increments().index();
+
+    table.text("name").notNullable();
+
+    table.float("latitude");
+
+    table.float("longitude");
+
+    table.text("state");
+
+    table.text("city");
+
+    table.text("zip");
+
+    table.text("county");
+
+    table.text("highway");
+  });
+};
+
+exports.down = function(knex, Promise) {
+  return knex.schema.dropTables("locations");
+};
