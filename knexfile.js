ad9e6b834753b3701f3d4ecaed5b199746f1e1d6
heysivani <heysivani@gmail.com>
2020-10-29 16:57:09 +0900

    updated knexfile and knex

--- knexfile.js
+++ knexfile.js
@@ -1,6 +1,11 @@
 // Update with your config settings.
 
 module.exports = {
+  client: "pg",
+  connection:
+    process.env.DATABASE_URL ||
+    `postgres://${process.env.USER}@127.0.0.1:5432/truckstop`,
+  searchPath: "public",
   development: {
     client: "pg",
     connection:
