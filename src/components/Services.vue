f6dffe36a2d1c48f02eda081795edd28e8738cdd
tlightk <klayxi@gmail.com>
2020-10-31 23:58:08 +0900

    updated services to v-bind to respective filter booleans

--- src/components/Services.vue
+++ src/components/Services.vue
@@ -1,23 +1,39 @@
 <template>
   <div class="services-container">
-    <input @change="updateOilChangefilter" type="checkbox" name="oil-change" />
+    <input
+      @change="updateOilChangefilter"
+      v-bind:checked="oilChangefilter"
+      type="checkbox"
+      name="oil-change"
+    />
     <label for="oil-change">Oil Change</label>
 
     <input
       @change="updateLightMechanicalfilter"
+      v-bind:checked="lightMechanicalfilter"
       type="checkbox"
       name="light-mechanical"
     />
     <label for="light-mechanical">Light Mechanical</label>
 
-    <input @change="updateTirePassfilter" type="checkbox" name="tire-pass" />
+    <input
+      @change="updateTirePassfilter"
+      v-bind:checked="tirePassfilter"
+      type="checkbox"
+      name="tire-pass"
+    />
     <label for="tire-pass">Tire Pass</label>
   </div>
 </template>
 
 <script>
+import { mapState } from "vuex";
+
 export default {
   name: "services",
+  computed: {
+    ...mapState(["oilChangefilter", "lightMechanicalfilter", "tirePassfilter"]),
+  },
   methods: {
     updateOilChangefilter() {
       this.$store.dispatch("changeOilChangefilter");
