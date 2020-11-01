73b32963ea8096db9a417738a882750dba2924b6
heysivani <heysivani@gmail.com>
2020-11-01 15:54:28 +0900

    made 1 test work! yay

--- tests/unit/HelloWorld.spec.js
+++ tests/unit/HelloWorld.spec.js
@@ -1,13 +1,31 @@
 import { expect } from "chai";
 import { shallowMount } from "@vue/test-utils";
-import HelloWorld from "@/components/HelloWorld.vue";
+import App from "@/App.vue";
+// import Searchbox from "@/components/Searchbox.vue";
+// import Locations from "@/components/Locations.vue";
+// import Services from "@/components/Services.vue";
+// import Amenities from "@/components/Amenities.vue";
+// import Restaurants from "@/components/Restaurants.vue";
 
-describe("HelloWorld.vue", () => {
-  it("renders props.msg when passed", () => {
-    const msg = "new message";
-    const wrapper = shallowMount(HelloWorld, {
-      propsData: { msg }
+describe("App.vue", () => {
+  let wrapper;
+  beforeEach(() => {
+    wrapper = shallowMount(App, {
+      computed: {
+        currentNavIndex() {
+          return 1;
+        },
+      },
     });
-    expect(wrapper.text()).to.include(msg);
+  });
+
+  it("should contain 3 div element children", () => {
+    const divs = wrapper.findAll("div");
+    expect(divs.length).to.equal(3);
   });
 });
+
+// //expect(vm.$el.querySelector('.title').textContent)
+// .toBe('todos');
+// expect(vm.$el.querySelector('.new-todo').placeholder)
+//   .toBe('What needs to be done?');
